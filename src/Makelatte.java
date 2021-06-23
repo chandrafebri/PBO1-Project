62097db9db14fa4565dda2577275052e01768f39
chandra febri <chandra.febri14@gmail.com>
2021-06-23 20:39:23 +0800

    Buat SubClass 3 Atribut 2 Method

---
+++ src/Makelatte.java
@@ -0,0 +1,12 @@
+public class Makelatte extends Barista {
+    public String es;
+    public String krimer;
+    public String taro;
+
+    public void latte () {
+        System.out.println ("Membuat Kopi Latte");
+    }
+    public void taroLatte () {
+        System.out.println ("Membuat Kopi Taro Latte");
+    }
+}
