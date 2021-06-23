2b4b7a9956815a7a896595c4e9e9724ddaabfbdd
chandra febri <chandra.febri14@gmail.com>
2021-06-23 21:29:29 +0800

    Buat 2 Object Tiap Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Barista kopi = new Barista();
+        Barista susu = new Barista("Hidupkan","Putarkan","Panaskan","Foam","Hancurkan","Tuangkan","Masak","Takarkan");
+        Barista kopisusu = new Barista("Grind","Steam","coffebean","milk");
     }
 }
