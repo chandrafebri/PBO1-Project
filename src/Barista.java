b6f9644549c40097dc1ffb76bccf9dde7cd9f446
chandra febri <chandra.febri14@gmail.com>
2021-06-23 20:30:59 +0800

    Buat 8 Class, 4 Method

---
+++ src/Barista.java
@@ -0,0 +1,23 @@
+public class Barista {
+        public String mesin;
+        public String grinder;
+        public String ketel;
+        public String steamer;
+        public String bijikopi;
+        public String susu;
+        public String air;
+        public String gula;
+
+        public void grindBiji () {
+            System.out.println("Menghancurkan biji kopi menjadi bubuk");
+        }
+        public void steamSusu () {
+            System.out.println("Susu telah menjadi foam");
+        }
+        public void makeEspresso () {
+            System.out.println("Espresso sudah siap");
+        }
+        public void makeKopisusu () {
+            System.out.println("Kopi Susu sudah siap");
+        }
+}
