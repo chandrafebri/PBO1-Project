1f33a4c5527608836d1e1c9dfbe1b356efbc1499
chandra febri <chandra.febri14@gmail.com>
2021-06-23 21:43:01 +0800

    Getter dan Setter class 2

--- src/Makelatte.java
+++ src/Makelatte.java
@@ -1,12 +1,36 @@
 public class Makelatte extends Barista {
-    public String es;
-    public String krimer;
-    public String taro;
+    private String es;
+    private String krimer;
+    private String taro;
 
     public void latte () {
         System.out.println ("Membuat Kopi Latte");
     }
     public void taroLatte () {
         System.out.println ("Membuat Kopi Taro Latte");
+    }
+
+    public String getEs() {
+        return es;
+    }
+
+    public void setEs(String es) {
+        this.es = es;
+    }
+
+    public String getKrimer() {
+        return krimer;
+    }
+
+    public void setKrimer(String krimer) {
+        this.krimer = krimer;
+    }
+
+    public String getTaro() {
+        return taro;
+    }
+
+    public void setTaro(String taro) {
+        this.taro = taro;
     }
 }
